7b81709a5f1f22e9c5c429e3ec78711ab377f746
Anton Holmquist <anton.holmquist@gmail.com>
2014-10-12 18:41:46 +0200

    Format

--- app.go
+++ app.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
-    "github.com/gorilla/mux"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"os"
 )
 
 func main() {
 
-	port := os.Getenv("PORT");
+	port := os.Getenv("PORT")
 
 	if len(port) == 0 {
 		port = "3000"
-	}	
+	}
 
 	// gorilla/mux is a powerful URL router and dispatcher.
 	router := mux.NewRouter().PathPrefix("/v1").Subrouter()
 
-	router.HandleFunc("/", func (res http.ResponseWriter, req *http.Request) {
+	router.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		res.Header().Set("Content-Type", "text/plain")
 		res.Write([]byte("This is an example server."))
 	})
 
-	router.HandleFunc("/users/{userID}", func (res http.ResponseWriter, req *http.Request) {
+	router.HandleFunc("/users/{userID}", func(res http.ResponseWriter, req *http.Request) {
 
 		vars := mux.Vars(req)
 		userID := vars["userID"]
@@ -32,7 +32,7 @@ func main() {
 		res.Write([]byte("user id: " + userID))
 	})
 
-    http.Handle("/", router)
-    http.ListenAndServe(":" + port, nil)
-    fmt.Println("Starting server on port", port)
+	http.Handle("/", router)
+	http.ListenAndServe(":"+port, nil)
+	fmt.Println("Starting server on port", port)
 }
