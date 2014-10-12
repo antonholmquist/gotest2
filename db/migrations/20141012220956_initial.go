58a68f64c9e21200517810b0bb6517a18e06ad4a
Anton Holmquist <anton.holmquist@gmail.com>
2014-10-12 23:14:16 +0200

    Add test migration

---
+++ db/migrations/20141012220956_initial.go
@@ -0,0 +1,23 @@
+
+package main
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+// Up is executed when this migration is applied
+func Up_20141012220956(txn *sql.Tx) {
+
+	fmt.Println("Hello from migration Up_20141012220956 Up!")
+
+	txn.Exec("CREATE TABLE user2 (name text);")
+
+}
+
+// Down is executed when this migration is rolled back
+func Down_20141012220956(txn *sql.Tx) {
+	 fmt.Println("Hello from migration Down_20141012220956 Down!")
+
+	 txn.Exec("DROP TABLE user2")
+}
