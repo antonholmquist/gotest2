6d26c4daeadeb7feece72c19b72d95a5f4db49af
Anton Holmquist <anton.holmquist@gmail.com>
2014-10-12 16:20:44 +0200

    Add dynamic port support

--- app.go
+++ app.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     "net/http"
+    "os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -10,6 +11,15 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	port := os.Getenv("PORT");
+
+	if len(port) == 0 {
+		port = "3000"
+	}
+
+	fmt.Println("PORT:", port)
+
     http.HandleFunc("/", handler)
     http.ListenAndServe(":3000", nil)
 }
